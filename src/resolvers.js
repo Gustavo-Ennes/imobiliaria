98e0ee3725ee9f6590b6dbc5d4330d5641a3cdb2
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-09-09 11:34:03 -0300

    rebuilded to match graphql.js

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,41 +1,216 @@
-const landResolvers = require('./models/Land').resolvers
-const propertyResolvers = require('./models/Property').resolvers
-const tenantResolvers = require("./models/Tenant").resolvers
-const ownerResolvers = require("./models/Owner").resolvers
-const landMutations = require('./models/Land').mutations
-const propertyMutations = require('./models/Property').mutations
-const tenantMutations = require("./models/Tenant").mutations
-const ownerMutations = require("./models/Owner").mutations
-const {loginResolver, logoutResolver, signInResolver} = require("./middleware/index")
+const { passwordMatch, encrypt } = require('../utils/crypt')
+const Land = require("./models/Land")
+const Owner = require("./models/Owner")
+const Property = require("./models/Property")
+const Tenant = require('./models/Tenant')
 
-
-
-module.exports = {
-  Query:{
-    lands:  landResolvers.lands,
-    landById: landResolvers.landById,
-    properties: propertyResolvers.properties,
-    propertyById: propertyResolvers.propertyById,
-    tenants: tenantResolvers.tenants,
-    tenantById: tenantResolvers.tenantsById,
-    owners: ownerResolvers.owners,
-    ownerById: ownerResolvers.ownerById
+const resolvers = {
+  lands: async(args, request) => {
+    try{
+        return await Land.find()
+      }catch(err){
+        console.log(err)
+    }
   },
-  Mutation:{
-    createLand: landMutations.create,
-    updateLand: landMutations.update,
-    deleteLand: landMutations.delete,
-    createProperty: propertyMutations.create,
-    updateProperty: propertyMutations.update,
-    deleteProperty: propertyMutations.delete,
-    createOwner: ownerMutations.create,
-    updateOwner: ownerMutations.update,
-    deleteOwner: ownerMutations.delete,
-    createTenant: tenantMutations.create,
-    updateTenant: tenantMutations.update,
-    deleteTenant: tenantMutations.delete,
-    login: loginResolver,
-    logout: logoutResolver,
-    signIn: signInResolver
+  landById: async(args, request) => {
+    const land = await Land.findOne({_id: args.id})
+    return land
+  },  
+  createLand: async(args, request) => {
+    try{
+      await Land.create(args.input)
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  updateLand: async(args, request) => {
+    try{
+      const land = await Land.findOne({_id: args.id})
+      await Land.updateOne({_id: args.id}, {$set: args.input})
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  deleteLand: async(args, request) => {
+    try{
+      await Land.deleteOne({_id: args.id})
+      return `Land id:${args.id} deleted.`
+    }catch(err){
+      console.log(err)
+    }
+  },
+  owners: async(args, request) => {
+    try{
+      return await Owner.find()
+    }catch(err){
+      console.log(err)
+    }
+  },
+  ownerById: async(args,request) => {
+    try{
+      const owner = await Owner.findOne({_id: args.id})
+      return owner
+    }catch(err){
+      console.log(err)
+    }    
+  },
+  createOwner: async(args, request) => {
+    try{
+      await Owner.create(args.input)
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  updateOwner: async(args, request) => {
+    try{
+      const owner = await Owner.findOne({_id: args.id})
+      await Owner.updateOne({_id: args.id}, {$set: args.input})
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  deleteOwner: async (args, request) => {
+    try{
+      await Owner.deleteOne({_id: args.id})
+      return `Owner id:${args.id} deleted.`
+    }catch(err){
+      console.log(err)
+    }
+  },
+  properties: async(args, request) => {
+    try{
+      return await Property.find()
+    }catch(err){
+      console.log(err)
+    }
+  },
+  propertyById: async(args, request) => {
+    try{
+      const property = await Property.findOne({_id: args.id})
+      return property
+    }catch(err){
+      console.log(err)
+    }
+  },
+  createProperty: async(args, request) => {
+    try{
+      await Property.create(args.input)
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  updateProperty: async(args, request) => {
+    try{
+      const property = await Property.findOne({_id: args.id})
+      await Property.updateOne({_id: args.id}, {$set: args.input})
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  deleteProperty: async (args, request) => {
+    try{
+      await Property.deleteOne({_id: args.id})
+      return `Property id:${args.id} deleted.`
+    }catch(err){
+      console.log(err)
+    }
+  },
+  tenants: async(args, request) => {
+    try{
+      return await Tenant.find()
+    }catch(err){
+      console.log(err)
+    }
+  },
+  tenantsById: async(args, request) => {
+    try{
+      const tenant = await Tenant.findOne({_id: args.id})
+      return tenant
+    }catch(err){
+      console.log(err)
+    }
+  },
+  createTenant: async(args, request) => {
+    try{
+      await Tenant.create(args.input)
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  updateTenant: async(args, request) => {
+    try{
+      const tenant = await Tenant.findOne({_id: args.id})
+      await Tenant.updateOne({_id: args.id}, {$set: args.input})
+      return args.input
+    }catch(err){
+      console.log(err)
+    }
+  },
+  deleteTenant: async (args, request) => {
+    await Tenant.deleteOne({_id: args.id})
+    return `Tenant id:${args.id} deleted.`
+  },
+  login: async(args, request)=> {
+    let res = false
+    try{
+
+      if(request.session.userID){
+        res =  true
+      } else{
+        const user = await Tenant.findOne({username: args.username})
+
+        if(!user){
+          user = await Owner.findOne({username: args.username})
+
+          if(user){
+            request.session.username = user.username
+            request.session.save()
+            res = true
+          }
+        } else {
+          res = true
+        }
+      }
+    }catch(err){
+      console.log(err)
+    }
+    return res
+  },
+  logout: (args, request) => {
+    if(request.session){
+      request.session.destroy()
+    }
+    return true
+  },
+  signIn: async (args, request) => {
+    const input = args.input
+    const type = args.type
+
+    try{
+
+      input.password = await encrypt(input.password)
+
+      if(type === 'tenant'){  
+        await resolvers.createTenant(args, request)
+      }else if (type === 'owner'){
+        await resolvers.createOwner(args, request)
+      }else{
+        console.log("SignIn attempt failed: there's no such type of user.")
+        return false
+      }
+    }catch(err){
+      console.log(err)
+      return false
+    }    
+    return true
   }
 }
+
+module.exports = resolvers
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,8 +1,131 @@
-module.exports = [
-  require('./models/Land').typeDef,
-  require('./models/Property').typeDef,
-  require('./models/Owner').typeDef,
-  require('./models/Tenant').typeDef,
-  require('./inputs'),
-  require('./models/Query')
-]
+module.exports = `
+
+type Tenant{  
+  id: ID!
+  name: String
+  phone: String
+  username: String
+  password: String
+  cellphone: String
+  rented: [Property]
+  documents: [String]
+  address_street: String
+  address_number: Int
+  address_complementation: String
+  address_reference: String
+  address_district: String
+  address_city: String
+  address_zip: String
+  createdAt: String
+  updatedAt: String
+}
+
+
+type Owner{
+  id: ID!
+  name: String
+  phone: String
+  username: String
+  password: String
+  cellphone: String
+  properties: [Property]
+  lands: [Land]
+  documents: [String]
+  address_street: String
+  address_number: Int
+  address_complementation: String
+  address_reference: String
+  address_district: String
+  address_city: String
+  address_zip: String
+  createdAt: String
+  modifiedAt: String
+}
+
+
+type Land{
+  id: ID!
+  ownerId: ID!
+  size: Float
+  infrastructures: [String]
+  documents: [String]
+  address_street: String
+  address_number: Int
+  address_complementation: String
+  address_reference: String
+  address_district: String
+  address_city: String
+  address_zip: String
+  value_sell: Float
+  value_rentPercentage: Float
+}
+
+
+type Property{
+  id: ID!
+  ownerId: ID!
+  tenantId: ID
+  bedrooms: Int
+  bathrooms: Int
+  parkingSpaces: Int
+  size: Float
+  privateSize: Float
+  documents: [String]
+  characteristics: [String]
+  infrastructures: [String]
+  address_street: String
+  address_number: Int
+  address_complementation: String
+  address_reference: String
+  address_district: String
+  address_city: String
+  address_zip: String
+  value_rent: Float
+  value_sell: Float
+  value_rentPercentage: Float
+  value_sellPercentage: Float
+  views: Int
+  shares: Int
+  likes: Int
+}
+
+type Query{
+  tenants: [Tenant]
+  owners: [Owner]
+  properties: [Property]
+  lands: [Land]
+  tenantById(id: ID!): Tenant
+  ownerById(id: ID!): Owner
+  propertyById(id: ID!): Property   
+  landById(id: ID!): Land
+}
+
+type Mutation{
+
+  createProperty(input: PropertyCreateInput): Property
+  updateProperty(id:ID!, input: PropertyUpdateInput): Property
+  deleteProperty(id: ID): String
+
+  createLand(input: LandCreateInput): Land
+  updateLand(id: ID!, input: LandUpdateInput): Land
+  deleteLand(id: ID!): String
+
+  createOwner(input: OwnerCreateInput): Owner
+  updateOwner(id: ID!, input: OwnerUpdateInput): Owner
+  deleteOwner(id: ID!): String
+
+  createTenant(input: TenantCreateInput): Tenant
+  updateTenant(id: ID!, input:TenantUpdateInput): Tenant
+  deleteTenant(id: ID!): String
+
+  login(username: String, password: String): Boolean
+  logout(username: String): Boolean
+  # the type here is to say if it's an Owner or a Tenant
+  # implement this with sessions 
+  signIn(input: OwnerCreateInput, type: String): Boolean
+  
+}
+
+${require("./inputs")}
+
+`
