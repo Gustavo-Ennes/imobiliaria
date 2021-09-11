a8030a4c49d77a1b4b6708723f6bb1abb1a4941d
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-09-11 17:02:44 -0300

    bulk and crypt and jsonStringfy modules tests

---
+++ test/unitary/crypt.js
@@ -0,0 +1,24 @@
+const { getRounds } = require("bcrypt")
+const { expect } = require("chai")
+const { encrypt, passwordMatch } = require("../../utils/crypt")
+
+describe(" ~ Encryption", () => {
+
+  let password = 'kratos'
+
+  describe("passwordMatch", () => {
+
+    it("Should create a hash that matches 'kratos' string", () => {
+      const enc = encrypt(password)
+      expect(passwordMatch(password, enc))
+    })
+  })
+
+  describe("encrypt", () => {
+    it("Should get the number of rounds of a given hash", () => {
+      const realRounds = 10
+      const enc = encrypt(password)
+      expect(getRounds(enc)).to.equal(realRounds)
+    })
+  })
+})
---
+++ test/unitary/jsonStringify.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai')
+const stringify = require('../../utils/jsonStringify')
+
+describe(" ~ jsonStringify", () => {
+  it("Should remove quotes from the keys of a stringified json",() => {
+    const json = {
+      "key1": "value1",
+      "key2": "value2",
+      "key3": "value3",
+      "key4": 7,
+      "key5": null,
+      "favFriends": ["Kolade", "Nithya", "Dammy", "Jack"],
+      "favPlayers": {"one": "Kante", "two": "Hazard", "three": "Didier"}
+    }
+    const stringifiedJson = stringify(json)
+    console.log(stringifiedJson)
+    expect(stringifiedJson.indexOf('"key1"')).to.equal(-1)
+    expect(stringifiedJson.indexOf('key1')).to.not.equal(-1)
+    expect(stringifiedJson.indexOf('"favPlayers"')).to.equal(-1)
+    expect(stringifiedJson.indexOf('favPlayers')).to.not.equal(-1)
+    expect(stringifiedJson.indexOf('"three"')).to.equal(-1)
+    expect(stringifiedJson.indexOf('three')).to.not.equal(-1)
+
+  })
+})
