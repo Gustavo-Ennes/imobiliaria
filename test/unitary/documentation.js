65dee6a9007d9e7a54471b07cace693d43443f9d
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-09-13 16:00:39 -0300

    check if is pdf file

---
+++ test/unitary/documentation.js
@@ -0,0 +1,28 @@
+const { expect } = require("chai")
+const { checkPdf } = require("../../utils/documentation")
+
+describe(" ~ Documentation", () => {
+  describe("pdf validation", () => {
+
+    it("Should avoid pdf links without a .pdf extension", async() => {
+      const wrongLink = "https://www.falsepdf.com/falsepdf.pdv"
+      expect(await checkPdf(wrongLink)).to.equal(false)
+    })
+
+    it("Should avoid false links", async() => {
+      const wrongLink = "httpe://wwv.falsepdf.com/falsepdf.pdf"
+      expect(await checkPdf(wrongLink)).to.equal(false)
+    })
+
+    it("Should avoid a valid link, but with response code 200", async() => {
+      const wrongLink = "https://www.pdfgloballinks.uk/wtf.pdf"
+      expect(await checkPdf(wrongLink)).to.equal(false)
+    })
+
+    it("Should return true with a true link in argument", async() =>{
+      const rightLink = "https://fee.org/media/27008/discovering-ayn-rand.pdf"
+      expect(await checkPdf(rightLink)).to.equal(true)
+    })
+
+  })
+})
---
+++ utils/documentation.js
@@ -0,0 +1,25 @@
+const axios = require("axios")
+
+const isLink = (string) => {
+  return string.startsWith("http://") || string.startsWith('https://')
+}
+
+const isPdf = (string) => {
+  return string.endsWith('.pdf')
+}
+
+const linkExists = async(string) => {
+  try{
+    const res = await axios.get(string)
+    const strings = res.data.split('\n')
+    return strings[0].indexOf('%PDF') !== -1
+  }catch(err){
+    console.log(err)
+    return false
+  }
+}
+
+
+module.exports = {
+  checkPdf: linkExists
+}
