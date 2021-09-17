5cd3c0b0a177338f2434df579937a800a42e14e6
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-09-17 11:02:08 -0300

    validation to logged users

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,6 @@
 const Admin = require('../src/models/Admin')
 const Owner = require('../src/models/Owner')
+const Tenant = require("../src/models/Tenant")
 
 module.exports = {
   checkOwner: async(ownerId) => {
@@ -11,16 +12,23 @@ module.exports = {
     return (a instanceof Admin)
   },
   checkTenantByUsername: async(username) => {
-    if(username instanceof String){
+    if(typeof username === 'string'){
       const t = await Tenant.findOne({username})
       return (t instanceof Tenant)
     }
     return false
   },
   checkAdminByUsername: async(username) => {
-    if(username instanceof String){
+    if(typeof username === 'string'){
       const a = await Admin.findOne({username})
       return (a instanceof Admin)
+    }
+    return false
+  },
+  checkOwnerByUsername: async(username) => {
+    if(typeof username === 'string'){
+      const o = await Owner.findOne({username})
+      return (o instanceof Owner)
     }
     return false
   }
